9d1960442655596b9bf31dc4a2c38279e724b9f5
Gevorg Harutyunyan <gevharut@gmail.com>
2016-05-12 02:00:01 +0400

    Clean-up

--- src/native/index.js
+++
@@ -1,9 +0,0 @@
-// Bootstrap environment
-require('react-native-browser-polyfill');
-require('../server/intl/polyfillLocales')(
-  self,
-  require('./config').default.locales
-);
-// TODO: Consider.
-// self.Promise = require('../common/configureBluebird');
-require('./main');
